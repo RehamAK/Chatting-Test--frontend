97f1ab97064b41ed31844d0be7acf3ad57ae4d24
RehamAK <49317311+RehamAK@users.noreply.github.com>
2019-12-30 19:55:26 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink  } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { BrowserRouter } from 'react-router-dom'
+import { setContext } from 'apollo-link-context'
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:8000/'
+});
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:8000/graphql/`,
+  options: {
+    reconnect: true
+  }
+});
+
+
+const link = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <ApolloProvider client={client}>
+      App
+    </ApolloProvider>
+  </BrowserRouter>
   );
 }
 
